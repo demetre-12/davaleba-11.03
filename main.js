cf05f86c5f3bda1e2a92fbe0d12d28bdf5567013
demetre-12 <demetredemetradze9@gmail.com>
2026-03-19 12:32:40 +0400

    commit-4

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
 function calculate() {
   let x = document.getElementById("numberF").value;
-  let op = document.getElementById("op").value
+  let op = document.getElementById("op").value;
   let y = document.getElementById("numberS").value;
+  
   x = parseInt(x);
   y = parseInt(y);
   let e = document.getElementById("ans");
+  let box = document.getElementById("box");
 
+  let res = 0;
   switch (op) {
     case "+":
-      e.textContent = x + y;
+      res = x + y;
+      e.textContent = res;
+
       break;
     case "-":
-      e.textContent = x - y;
+      res = x - y;
+      e.textContent = res;
+
       break;
     case "*":
-      e.textContent = x * y;
+      res = x * y;
+      e.textContent = res;
+
       break;
     case "/":
-      e.textContent = x / y;
+      res = x / y;
+      e.textContent = res;
+
       break;
+  }
+
+  if ((res) => 0) {
+    box.textContent = "დადებიათია";
+    box.style.backgroundColor = "Green";
+  } else {
+    box.textContent = "უარყოფიოთია";
+    box.style.backgroundColor = "Red";
   }
 }
