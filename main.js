7cd205c0f945f99ce42853a35932f01ee745e72c
demetre-12 <demetredemetradze9@gmail.com>
2026-03-19 11:25:03 +0400

    commit-1

---
+++ main.js
@@ -0,0 +1,25 @@
+function calculate() {
+  let x = document.getElementById("numberF").value;
+  let op = document.getElementById("op").value
+  let y = document.getElementById("numberS").value;
+  x = parseInt(x);
+  y = parseInt(y);
+  let e = document.getElementById("ans");
+
+  switch (op) {
+    case "+":
+      e.textContent = x + y;
+      break;
+    case "-":
+      e.textContent = x - y;
+      break;
+    case "*":
+      e.textContent = x * y;
+      break;
+    case "/":
+      e.textContent = x / y;
+      break;
+  }
+
+  writeDown(x, y);
+}
